b47aef7210eb15a2d11550f16f1ceb88338ece2d
Venkat Korapaty <venkat.korapaty@gmail.com>
2018-11-06 21:18:12 -0500

    made changes to formating data

--- format_data.py
+++ format_data.py
@@ -1,24 +1,72 @@
+import numpy as np
 import os
 from sklearn.feature_extraction.text import CountVectorizer
 
 def main():
     path = "./aclImdb/train/pos/"
     reviews = os.listdir(path)
-    vectorizer = CountVectorizer()
+    stop_words = ["10", "a", "about", "above", "across", "after", "afterwards", "again", "against",
+    "all", "almost", "alone", "along", "already", "also", "although", "always",
+    "am", "among", "amongst", "amoungst", "amount", "an", "and", "another",
+    "any", "anyhow", "anyone", "anything", "anyway", "anywhere", "are",
+    "around", "as", "at", "back", "be", "became", "because", "become",
+    "becomes", "becoming", "been", "before", "beforehand", "behind", "being",
+    "below", "beside", "besides", "between", "beyond", "bill", "both",
+    "bottom", "br", "but", "by", "call", "can", "cannot", "cant", "co", "con",
+    "could", "couldnt", "de", "describe", "detail", "do", "done",
+    "down", "due", "during", "each", "eg", "eight", "either", "eleven", "else",
+    "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone",
+    "everything", "everywhere", "except", "few", "fifteen", "fifty", "fill",
+    "find", "fire", "first", "five", "for", "former", "formerly", "forty",
+    "found", "four", "from", "front", "full", "further", "get", "give", "go",
+    "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter",
+    "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his",
+    "how", "however", "hundred", "i", "ie", "if", "in", "inc", "indeed",
+    "interest", "into", "is", "it", "its", "its", "itself", "keep", "last", "latter",
+    "latterly", "least", "less", "ltd", "made", "many", "may", "me",
+    "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly",
+    "move", "much", "must", "my", "myself", "name", "namely", "neither",
+    "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone",
+    "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on",
+    "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our",
+    "ours", "ourselves", "out", "over", "own", "part", "per", "perhaps",
+    "please", "put", "rather", "re", "same", "see", "seem", "seemed",
+    "seeming", "seems", "serious", "several", "she", "should", "show", "side",
+    "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone",
+    "something", "sometime", "sometimes", "somewhere", "still", "such",
+    "system", "take", "ten", "than", "that", "the", "their", "them",
+    "themselves", "then", "thence", "there", "thereafter", "thereby",
+    "therefore", "therein", "thereupon", "these", "they", "thick", "thin",
+    "third", "this", "those", "though", "three", "through", "throughout",
+    "thru", "thus", "to", "together", "too", "top", "toward", "towards",
+    "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us",
+    "very", "via", "was", "we", "well", "were", "what", "whatever", "when",
+    "whence", "whenever", "where", "whereafter", "whereas", "whereby",
+    "wherein", "whereupon", "wherever", "whether", "which", "while", "whither",
+    "who", "whoever", "whole", "whom", "whose", "why", "will", "with",
+    "within", "without", "would", "yet", "you", "your", "yours", "yourself",
+    "yourselves"]
+    vectorizer = CountVectorizer(stop_words=stop_words)
 
-    for review in reviews:
-        print(review)
-        text = open(path + review).readlines()
-        print(text)
+    all_reviews = []
+    for i in range(len(reviews)):
+        print(i)
+        text = open(path + reviews[i]).readlines()[0]
+        # print(text)
+        all_reviews.append(text)
 
         # need to add each text value into overall array
-        x = vectorizer.fit_transform(text)
-        print(x)
+        # print(x)
         # gets the numpy array matrix
-        x.toarray()
-        # gets what word each feature in the matrix is
-        vectorizer.get_feature_names()
-        break
+        # x.toarray()
+
+    x = vectorizer.fit_transform(all_reviews)
+    # gets what word each feature in the matrix is
+    names = vectorizer.get_feature_names()
+    print(names)
+    print(x.shape)
+    np.save('./train_pos.npy', x.toarray()[:1000])
+    np.save('./train_pos_words.npy', np.array(names)[:100])
 
 
 if __name__ == '__main__':
