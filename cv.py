e25c688ffeb49bb0192b60ade7b712530d69b086
venkatkorapaty <venkat.korapaty@gmail.com>
2018-11-12 13:17:24 -0500

    fixed bug

--- cv.py
+++ cv.py
@@ -13,12 +13,12 @@ def main():
     X_cv = np.empty([size_of_cv, amount_of_words])
     y_cv = np.empty([size_of_cv, 1])
 
-    zeros = 0
+    zeroes = 0
     ones = 0
 
     for i in range(size_of_cv):
         print(i)
-        rand_review = random.randint(0, amount_of_data)
+        rand_review = random.randint(0, amount_of_data - 1)
 
         X_cv[i, :] = X[rand_review, :]
         X = np.delete(X, i, 0)
@@ -28,16 +28,16 @@ def main():
             y_cv[i, :] = 1
             middle = middle - 1
         else:
-            zeros = zeros + 1
+            zeroes = zeroes + 1
             y_cv[i, :] = 0
-        
+
         amount_of_data = amount_of_data - 1
-    
+
+    np.save('./cv_only_reduced.npy', X_cv)
+    np.save('./train_only_reduced.npy', X)
     print("Zeroes: ", zeroes, " Ones: ", ones, " middle: ", middle)
     print("X: ", X.shape, " amount of data: ", amount_of_data)
     print("X_cv: ", X_cv.shape)
-    np.save('./cv_reduced2.npy', X_cv)
-
 
 if __name__ == '__main__':
     main()
