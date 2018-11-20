d1cec3270ea37b958235716cd5de6ae2f18781af
venkatkorapaty <venkat.korapaty@gmail.com>
2018-11-20 16:28:04 -0500

    doing feature selection

--- log_reg_lmrd.py
+++ log_reg_lmrd.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
+from sklearn.feature_selection import RFE
 
 def main():
     X_t = np.load('lmrd_train.npy')
@@ -8,19 +9,20 @@ def main():
     y_cv = np.load('lmrd_cv_y.npy')
 
     LogisReg(X_t, y_t, X_cv, y_cv, C=1)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.95)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.9)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.85)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.8)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.75)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.7)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.65)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.6)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.55)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.5)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.3)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.1)
-    LogisReg(X_t, y_t, X_cv, y_cv, C=0.007)
+    recursive_feature_elimination(X_t, y_t, X_cv, y_cv)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.95)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.9)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.85)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.8)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.75)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.7)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.65)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.6)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.55)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.5)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.3)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.1)
+    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.007)
 
 def LogisReg(X_t, y_t, X_cv, y_cv, C=1):
     lr = LogisticRegression(C=C)
@@ -33,6 +35,19 @@ def LogisReg(X_t, y_t, X_cv, y_cv, C=1):
     
     correct_preds = np.array(cv_pred) == y_cv[:, 0]
     print("CV error: ", np.sum(correct_preds) / y_cv[:, 0].shape[0])
+
+def recursive_feature_elimination(X_t, y_t, X_cv, y_cv):
+    lr = LogisticRegression()
+    selector = RFE(lr, 10000, step=1).fit(X_t, y_t)
+
+    train_pred = selector.predict(X_t)
+    cv_pred = selector.predict(X_cv)
+    print("C: ", C)
+    correct_preds = np.array(train_pred) == y_t[:, 0]
+    print("Train error: ", np.sum(correct_preds) / y_t[:, 0].shape[0])
+    
+    correct_preds = np.array(cv_pred) == y_cv[:, 0]
+    print("CV error: ", np.sum(correct_preds) / y_cv[:, 0].shape[0])    
 
 
 if __name__ == '__main__':
