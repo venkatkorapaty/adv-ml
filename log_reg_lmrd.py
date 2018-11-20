58c9b0048e8a482bca748af642c84ab8b17b575c
venkatkorapaty <venkat.korapaty@gmail.com>
2018-11-20 00:34:09 -0500

    testing different regularization levels

--- log_reg_lmrd.py
+++ log_reg_lmrd.py
@@ -7,18 +7,32 @@ def main():
     X_cv = np.load('lmrd_cv.npy')
     y_cv = np.load('lmrd_cv_y.npy')
 
-    lr = LogisticRegression()
+    LogisReg(X_t, y_t, X_cv, y_cv, C=1)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.95)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.9)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.85)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.8)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.75)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.7)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.65)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.6)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.55)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.5)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.3)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.1)
+    LogisReg(X_t, y_t, X_cv, y_cv, C=0.007)
+
+def LogisReg(X_t, y_t, X_cv, y_cv, C=1):
+    lr = LogisticRegression(C=C)
     lr.fit(X_t, y_t[:, 0])
-    print(lr)
     train_pred = lr.predict(X_t)
     cv_pred = lr.predict(X_cv)
-    
+    print("C: ", C)
     correct_preds = np.array(train_pred) == y_t[:, 0]
     print("Train error: ", np.sum(correct_preds) / y_t[:, 0].shape[0])
     
     correct_preds = np.array(cv_pred) == y_cv[:, 0]
     print("CV error: ", np.sum(correct_preds) / y_cv[:, 0].shape[0])
-    
 
 
 if __name__ == '__main__':
