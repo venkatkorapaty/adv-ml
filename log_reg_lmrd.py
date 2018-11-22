9a1239c736fa7b804cc7216eb4eff9119dfba0a1
venkatkorapaty <venkat.korapaty@gmail.com>
2018-11-22 00:22:55 -0500

    using l1 reg

--- log_reg_lmrd.py
+++ log_reg_lmrd.py
@@ -8,24 +8,26 @@ def main():
     X_cv = np.load('lmrd_cv.npy')
     y_cv = np.load('lmrd_cv_y.npy')
 
-    LogisReg(X_t, y_t, X_cv, y_cv, C=1)
-    recursive_feature_elimination(X_t, y_t, X_cv, y_cv)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.95)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.9)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.85)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.8)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.75)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.7)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.65)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.6)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.55)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.5)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.3)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.1)
-    #LogisReg(X_t, y_t, X_cv, y_cv, C=0.007)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=1)
+    #recursive_feature_elimination(X_t, y_t, X_cv, y_cv, p='l1', C=0.75)
+    #recursive_feature_elimination(X_t, y_t, X_cv, y_cv, p='l1', C=0.5)
+    recursive_feature_elimination(X_t, y_t, X_cv, y_cv, p='l1', C=0.3)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.95)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.9)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.85)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.8)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.75)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.7)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.65)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.6)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.55)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.5)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.3)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.1)
+    #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.007)
 
-def LogisReg(X_t, y_t, X_cv, y_cv, C=1):
-    lr = LogisticRegression(C=0.75)
+def LogisReg(X_t, y_t, X_cv, y_cv, p, C=1):
+    lr = LogisticRegression(penalty=p, C=C)
     lr.fit(X_t, y_t[:, 0])
     train_pred = lr.predict(X_t)
     cv_pred = lr.predict(X_cv)
@@ -36,9 +38,9 @@ def LogisReg(X_t, y_t, X_cv, y_cv, C=1):
     correct_preds = np.array(cv_pred) == y_cv[:, 0]
     print("CV error: ", np.sum(correct_preds) / y_cv[:, 0].shape[0])
 
-def recursive_feature_elimination(X_t, y_t, X_cv, y_cv):
-    lr = LogisticRegression()
-    selector = RFE(lr, 10000, step=100).fit(X_t, y_t[:, 0])
+def recursive_feature_elimination(X_t, y_t, X_cv, y_cv, p, C=1):
+    lr = LogisticRegression(penalty=p, C=C)
+    selector = RFE(lr, 10000, step=50).fit(X_t, y_t[:, 0])
 
     train_pred = selector.predict(X_t)
     cv_pred = selector.predict(X_cv)
