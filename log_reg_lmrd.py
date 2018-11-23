c42cc06f77c245875fd5ebe3e35fede34e902c03
venkatkorapaty <venkat.korapaty@gmail.com>
2018-11-23 02:13:12 -0500

    used PCA for 95% variance and used KFold

--- log_reg_lmrd.py
+++ log_reg_lmrd.py
@@ -1,6 +1,7 @@
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.feature_selection import RFE
+from sklearn.model_selection import KFold
 
 def main():
     X_t = np.load('lmrd_train.npy')
@@ -25,6 +26,92 @@ def main():
     #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.3)
     #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.1)
     #LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=0.007)
+
+def main2():
+    X = np.load('lmrd_train_0-95.npy')
+    y_t = np.load('lmrd_train_y.npy')
+    y_cv = np.load('lmrd_cv_y.npy')
+    y = np.concatenate((y_t, y_cv))
+
+    # KFoldNoRegLogisticRegression(X, y, 5, C=1)
+    """ RESULTS:
+    Split : 1
+    C:  1
+    Train error:  0.9677
+    CV error:  0.8762
+    Split : 2
+    C:  1
+    Train error:  0.96915
+    CV error:  0.861
+    Split : 3
+    C:  1
+    Train error:  0.97075
+    CV error:  0.8706
+    Split : 4
+    C:  1
+    Train error:  0.97065
+    CV error:  0.8616
+    Split : 5
+    C:  1
+    Train error:  0.9698
+    CV error:  0.8714
+    """
+    #KFoldNoRegLogisticRegression(X, y, 5, C=0.75)
+    """ RESULTS:
+    Split : 1
+    C:  0.75
+    Train error:  0.9602
+    CV error:  0.8776
+    Split : 2
+    C:  0.75
+    Train error:  0.96305
+    CV error:  0.8674
+    Split : 3
+    C:  0.75
+    Train error:  0.9639
+    CV error:  0.8742
+    Split : 4
+    C:  0.75
+    Train error:  0.964
+    CV error:  0.8672
+    Split : 5
+    C:  0.75
+    Train error:  0.964
+    CV error:  0.8754
+    """
+    KFoldNoRegLogisticRegression(X, y, 5, C=0.5)
+    """RESULTS:
+    Split : 1
+    C:  0.5
+    Train error:  0.95055
+    CV error:  0.8816
+    Split : 2
+    C:  0.5
+    Train error:  0.95375
+    CV error:  0.8706
+    Split : 3
+    C:  0.5
+    Train error:  0.9524
+    CV error:  0.88
+    Split : 4
+    C:  0.5
+    Train error:  0.9539
+    CV error:  0.8736
+    Split : 5
+    C:  0.5
+    Train error:  0.95445
+    CV error:  0.8806
+    """
+
+def KFoldNoRegLogisticRegression(X, y, n_splits=2, C=1):
+    kf = KFold(n_splits=n_splits)
+    split = 1
+    for train_index, cv_index in kf.split(X):
+        print("Split :", split)
+        X_t, X_cv = X[train_index], X[cv_index]
+        y_t, y_cv = y[train_index], y[cv_index]
+        LogisReg(X_t, y_t, X_cv, y_cv, p='l1', C=C)
+        split += 1
 
 def LogisReg(X_t, y_t, X_cv, y_cv, p, C=1):
     lr = LogisticRegression(penalty=p, C=C)
@@ -52,4 +139,5 @@ def recursive_feature_elimination(X_t, y_t, X_cv, y_cv, p, C=1):
 
 
 if __name__ == '__main__':
-    main()
+    #main()
+    main2()
--- visualise.py
+++ visualise.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib
-matplotlib.use('TkAgg')
+#matplotlib.use('TkAgg')
 from scipy.sparse import coo_matrix
 import scipy.sparse as ss
 import matplotlib.pyplot as plt
@@ -72,8 +72,49 @@ def main3():
     plt.savefig('DGFGFGF.png')
     plt.show()
 
+def main4():
+    X_t = np.load('lmrd_train.npy')
+    y_t = np.load('lmrd_train_y.npy')
+    V = np.load('lmrd_eigens.npy')
+    print("Finished loading..")
+    X_t = (X_t - X_t.mean(axis=0)).dot(V.T[:, :3])
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    for i in range(X_t.shape[0]):
+        print(i)
+        if y_t[i] == 1:
+            c = 'r'
+            m = 'o'
+        else:
+            c = 'b'
+            m = '^'
+        ax.scatter(X_t[i, 0], X_t[i, 1], X_t[i, 2], c=c, marker=m)
+
+    ax.set_xlabel('1st Principle Component')
+    ax.set_ylabel('2nd Principle Component')
+    ax.set_zlabel('3rd Principle Component')
+
+    plt.savefig('DGFGFGF.png')
+    plt.show()
+
+def main5():
+    X = np.load('lmrd_train.npy')
+    X_cv = np.load('lmrd_cv.npy')
+    print("Loaded data..")
+    X = np.concatenate((X, X_cv))
+    X_cv = None
+    
+    pca = PCA(n_components=0.95)
+    X_reduced = pca.fit_transform(X)
+    print("PCA finished..")
+    np.save('lmrd_train_0-95.npy', X_reduced)
+    print(X_reduced.shape)
 
 if __name__ == "__main__":
     # main()
     # main2()
-    main3()
+    # main3()
+    # main4()
+    main5()
